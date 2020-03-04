610b33bac45b08aef5d0de24b12f17d35cd072a5
William Kent <wjk011@gmail.com>
2020-03-04 16:57:13 -0500

    Add loader_fatal() function

---
+++ loader_tool/fatal.c
@@ -0,0 +1,14 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void loader_fatal(const char *msg, ...) __attribute__((noreturn)) {
+	char *message = NULL;
+
+	va_list ap;
+	va_start(ap, msg);
+	vasprintf(&message, msg, ap);
+	va_end(ap);
+
+	abort();
+}
